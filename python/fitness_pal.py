9de945e68581b575f9f2c99d419d42f3346964fe
Theodore Li <theodoreqili@gmail.com>
2020-01-25 16:53:34 -0600

    Fitness pal connected with getWaterData

--- python/fitness_pal.py
+++ python/fitness_pal.py
@@ -1,8 +1,10 @@
 import myfitnesspal
 import datetime
 import sys
+import re
+from parse_water_footprint import *
 
-def request_today(username):
+def request_today(username, parser):
     """
     To run:
     sudo pip insntall myfitnesspal
@@ -16,9 +18,23 @@ def request_today(username):
     food_ate = []
     for meal in day.meals:
         for item in meal:
-            food_ate.append(item.name)
+            # Split by name
+            split_1 = item.name.split(', ')
+
+            # split_2 ex: [3, 'stalk']
+            split_2 = split_1[1].split(' ')
+            quantity = float(split_2[0])
+            units = split_2[1]
+
+            food_ate.append(parser.get_water_data('110100', 
+                            name=split_1[0],
+                            quantity=quantity,
+                            units=units))
     
-    print(food_ate)
+    for data in food_ate:
+        print(data)
 
 if __name__ == '__main__':
-    request_today(sys.argv[1])
+    # Add your myfitnesspal username as an argument
+    parser = CSVParser()
+    request_today(sys.argv[1], parser)
