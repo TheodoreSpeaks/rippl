500e54ff9c18d0f2524950a9d48c91ee193af9d3
Arushi Rai <arushirai1999@gmail.com>
2020-01-25 21:29:02 -0500

    run flask

--- python/app.py
+++ python/app.py
@@ -1,11 +1,22 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 import barcode
 from parse_water_footprint import *
+import requests
+import json
 
 app = Flask(__name__)
 
 parser = CSVParser()
 
-@app.route('/barcode/<barcode_id>')
-def scan_barcode(barcode_id):
+@app.route('/heartbeat')
+def hello():
+    return 'hello'
+
+@app.route('/barcode')
+def scan_barcode():
+    barcode_id = str(request.args.get("barcode_id", ""))
+    #might need to use jsonify or .to_json
     return str(barcode.scan(barcode_id, parser))
+
+if __name__ == '__main__':
+	app.run(debug=True, port=5000)
